4e827b7c8ec4a758b29981df68014b27f0c3e6c4
Josh <jjswore@uw.edu>
2023-11-15 20:07:44 -0800

    added data and new directories

---
+++ tests/Format_CSV.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/joshswore/analysis_python_code/EAG_and_VOC_Project/'
+                 'MOX_Manduca_EAG_Analysis/Data/Raw/0727231505M1A1_1k_artCov_00_1.csv')
+
+df
